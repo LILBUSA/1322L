503788289921a65123002a2b85c4d24bf376165d
tblade <bigtay312@gmail.com>
2021-03-05 14:00:08 -0500

    lab8

--- Lab8/src/sample/Controller.java
+++ Lab8/src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public Label sumLabel;
+    public TextField num1;
+    public TextField num2;
+
+    public void add(ActionEvent actionEvent) {
+        int sum;
+        try {
+            sum = Integer.parseInt(num1.getText()) + Integer.parseInt(num2.getText());
+            sumLabel.setText(String.valueOf(sum));
+        }
+        catch (NumberFormatException e) {
+            sumLabel.setText("Please insert valid integers");
+        }
+    }
+
 }
