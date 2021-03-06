5f76019c75a9290db38db941d7457ca147e941bf
tblade <bigtay312@gmail.com>
2021-03-06 18:13:54 -0500

    practice

--- GUIPractice/src/sample/Main.java
+++ GUIPractice/src/sample/Main.java
@@ -5,15 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Group gc = new Group();
-        Scene scene = new Scene(gc, 300, 300);
+        Group root = new Group();
+        Canvas canvas = new Canvas(300, 200);
+        Scene scene = new Scene(root, 300, 275);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.fillOval(10, 60, 30, 30);
+        gc.strokeLine(40, 10, 10, 40);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
