064428e3011e17d86308259f6e3e41520e61c055
tblade <bigtay312@gmail.com>
2021-04-08 12:20:00 -0400

    init Lab12

---
+++ Lab12/src/Lab12.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Lab12 {
+    public static void main(String[] args) {
+        try {
+            String[] txtLine = new String[2];
+            File[] file = new File[] {new File("rn.txt"), new File("assets/rightnow.tx")};
+            Scanner[] scan = new Scanner[] {new Scanner(file[0]), new Scanner(file[1])};
+            int lineNum = 0;
+            while (scan[0].hasNextLine() || scan[1].hasNextLine()) {
+                if (scan[0].hasNextLine() && scan[1].hasNextLine()) {
+                    lineNum++;
+                    txtLine[0] = scan[0].nextLine();
+                    txtLine[1] = scan[1].nextLine();
+                    if (!(txtLine[0].equals(txtLine[1]))) {
+                        System.out.println("Line " + lineNum + "\n<" + txtLine[0] + "\n>" + txtLine[1]);
+                    }
+                } else {
+                    System.out.println("Files have different number of lines");
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
